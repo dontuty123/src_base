57b5077d287ecd42b9a5170f8cef793158445ae6
Pham thuc <phamtrithuc22022000@gmail.com>
2023-07-27 16:41:07 +0700

    a

---
+++ src/useRouteElement.jsx
@@ -0,0 +1,18 @@
+import MainLayout from './layout/MainLayout/MainLayout'
+import HomePage from './pages/HomePage/HomePage'
+import { useRoutes } from 'react-router-dom'
+
+export default function useRouteElement() {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      index: true,
+      element: (
+        <MainLayout>
+          <HomePage />,
+        </MainLayout>
+      ),
+    },
+  ])
+  return routeElements
+}
